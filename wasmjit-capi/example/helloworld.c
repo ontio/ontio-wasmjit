a02e25904c193ac424913786128286699e603bae
Lucas <sishsh@163.com>
2019-12-04 15:14:59 +0800

    add valgrind

--- wasmjit-capi/example/helloworld.c
+++ wasmjit-capi/example/helloworld.c
@@ -2,11 +2,12 @@
 #include "wasmjit.h"
 #include <string.h>
 
-void func_test(wasmjit_chain_context_t *ctx) {
+int func_test(wasmjit_chain_context_t *ctx) {
 	wasmjit_chain_context_set_gas(ctx, 99900000);
 	uint64_t gas = wasmjit_chain_context_get_gas(ctx);
 	if (gas!= 99900000) {
 		printf("wasmjit_chain_context_get_gas failed\n");
+		return -1;
 	}
 
 	address_t caller = {1};
@@ -16,7 +17,7 @@ void func_test(wasmjit_chain_context_t *ctx) {
 	for (int i=0;i<20;i++) {
 		if (caller[i] != caller2[i]) {
 			printf("num:%d, num2:%d\n",caller[i],caller2[i]);
-			break;
+            return -1;
 		}
 	}
 	wasmjit_bytes_t buf = wasmjit_bytes_new(20);
@@ -24,46 +25,48 @@ void func_test(wasmjit_chain_context_t *ctx) {
 	wasmjit_bytes_t buf2 = wasmjit_chain_context_take_output(ctx);
 	if (*buf.data != *buf2.data){
 		printf("wasmjit_chain_context_set_output failed\n");
+        return -1;
 	}
     char *file_path = "./wast/helloworld.wast";
 	wasmjit_slice_t name = {data:(uint8_t *)file_path,len:strlen(file_path)};
 	wasmjit_bytes_t wasm = wasmjit_test_read_wasm_file(name);
 	if (wasm.len == 0){
 	    printf("wasmjit_test_read_wasm_file failed\n");
-	    return;
+        return -1;
 	}
 	wasmjit_slice_t wasm_slice = wasmjit_bytes_as_slice(wasm);
-    res = wasmjit_validate(wasm_slice);
+    wasmjit_result_t res = wasmjit_validate(wasm_slice);
     if (res.kind != 0) {
         printf("wasmjit_validate failed");
-        return;
+        return -1;
     }
 	wasmjit_module_t *module;
-    wasmjit_result_t res = wasmjit_compile(&module,wasm_slice);
+    res = wasmjit_compile(&module,wasm_slice);
     if (res.kind != 0) {
         printf("wasmjit_compile failed\n");
-        return;
+        return -1;
     }
     wasmjit_resolver_t *resolver = wasmjit_simple_resolver_create();
     if (resolver == NULL) {
         printf("wasmjit_simple_resolver_create failed\n");
-        return;
+        return -1;
     }
     wasmjit_instance_t *instance;
     res = wasmjit_module_instantiate(module,resolver,&instance);
     if (res.kind != 0) {
         printf("wasmjit_module_instantiate failed\n");
-        return;
+        return -1;
     }
 
     res = wasmjit_instance_invoke(instance,ctx);
     if (res.kind != 0) {
         printf("wasmjit_instance_invoke failed:%d\n", res.kind);
-        return;
+        return -1;
     }
     wasmjit_module_destroy(module);
     wasmjit_instance_destroy(instance);
 	printf("success\n");
+    return 0;
 }
 
 int main() {
@@ -89,6 +92,5 @@ int main() {
 	height, &blockhash, timestamp, &txhash, callers, witness, input, gas_left, 2);
 
 	printf("ctx :%p\n", ctx);
-	func_test(ctx);
-	return 0;
+	return func_test(ctx);
 }
