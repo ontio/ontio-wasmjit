d3aa0909abccf96b11b36172214fc3877cdb9e55
laizy <laizy@users.noreply.github.com>
2019-12-18 10:36:42 +0800

    support trap test case in harness (#33)
    * code clean
    
    * support trap test case in harness

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use crate::chain_api::ChainCtx;
+use crate::chain_api::{ChainCtx, ChainResolver};
+use crate::error::Error;
+use crate::executor::build_module;
+
 pub mod chain_api;
 pub mod error;
 pub mod executor;
@@ -9,26 +12,37 @@ pub mod utils;
 
 pub mod disassm;
 
-/// Simple executor that assert the wasm file has an export function `invoke(a:i32, b:32)-> i32`.
-pub fn execute(wat: &str, func: &str, args: Vec<i64>, verbose: bool) -> Option<i64> {
-    let chain = ChainCtx::new(
-        1,
-        1u32,
-        [1u8; 32],
-        [1u8; 32],
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
-        Vec::new(),
-        0,
-    );
-    executor::execute(wat, func, args, verbose, chain)
+pub fn execute(
+    wat: &str,
+    chain: ChainCtx,
+    func: &str,
+    args: Vec<i64>,
+) -> Result<Option<i64>, Error> {
+    let wasm = wast::parse_str(wat).unwrap();
+    let module = build_module(&wasm).unwrap();
+
+    let mut resolver = ChainResolver;
+    let mut instance = module.instantiate(&mut resolver).unwrap();
+
+    instance.execute(chain, func, args)
 }
 
 #[test]
 fn test_chain2() {
     use chain_api::Address;
-    fn excute(method: &str) {
+    for method in [
+        "get_current_block_hash",
+        "get_current_tx_hash",
+        "get_timestamp",
+        "get_block_height",
+        "get_block_height",
+        "caller_address",
+        "entry_address",
+        "check_witness",
+        "sha256",
+    ]
+    .iter()
+    {
         let wat = include_str!("../tests/chain-api.wast");
         let callers: Vec<Address> = vec![[1u8; 20], [1u8; 20]];
         let witness: Vec<Address> = vec![[1u8; 20]];
@@ -43,24 +57,7 @@ fn test_chain2() {
             Vec::new(),
             0,
         );
-        let res: u64 = executor::execute(wat, "invoke", Vec::new(), false, chain).unwrap() as u64;
+        let res: u64 = execute(wat, chain, "invoke", Vec::new()).unwrap().unwrap() as u64;
         assert_eq!(res, 1);
     }
-    excute("get_current_block_hash");
-
-    excute("get_current_tx_hash");
-
-    excute("get_timestamp");
-
-    excute("get_block_height");
-
-    excute("get_block_height");
-
-    excute("caller_address");
-
-    excute("entry_address");
-
-    excute("check_witness");
-
-    excute("sha256");
 }
